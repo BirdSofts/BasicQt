b7a1b8ba6ddea3dcefe7f35ba1556b33e6510d58
Mehrdad Solimanimajd <s.mehrdad.47@outlook.com>
2019-09-11 15:56:12 +0430

    InitialUpload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿// *******************************************************************************************
+/// <summary>
+/// 
+/// </summary>
+/// <created>ʆϒʅ,10.09.2019</created>
+/// <changed>ʆϒʅ,11.09.2019</changed>
+// *******************************************************************************************
+
+
+#include <QApplication>
+
+#include "mainwindow.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+﻿// *******************************************************************************************
+/// <summary>
+/// 
+/// </summary>
+/// <created>ʆϒʅ,10.09.2019</created>
+/// <changed>ʆϒʅ,11.09.2019</changed>
+// *******************************************************************************************
+
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
