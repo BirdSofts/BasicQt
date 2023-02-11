860b5489b527dc68806f4e089466cdd3c14cddc4
Mehrdad Solimanimajd <s.mehrdad.47@outlook.com>
2023-02-12 00:27:45 +0330

    MacOsTest+Bugs+FileHeaders

--- gtests/gtests.h
+++ gtests/gtests.h
@@ -1,16 +1,19 @@
-﻿// *******************************************************************************************
+﻿
+// *******************************************************************************************
 /// <summary>
-/// 
+/// gtests.h
+/// QtIntroduction
+/// created by Mehrdad Solimanimajd on 15.09.2019
 /// </summary>
-/// <created>ʆϒʅ,15.09.2019</created>
-/// <changed>ʆϒʅ,16.09.2019</changed>
+/// <created>ʆϒʅ, 15.09.2019</created>
+/// <changed>ʆϒʅ, 11.02.2023</changed>
 // *******************************************************************************************
 
 #ifndef GTESTS_H
 #define GTESTS_H
 
 
-#include "gtest/gtest.h"
+#include "gtests/gtests.h"
 
 
 #endif //GTESTS_H
